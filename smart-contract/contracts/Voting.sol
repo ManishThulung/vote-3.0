a295c50044ea5f7025846f31631fcbf7c2593148
ManishThulung <manish.thulung58@gmail.com>
2024-01-30 23:47:39 +0545

    smart contract

--- smart-contract/contracts/Voting.sol
+++ smart-contract/contracts/Voting.sol
@@ -24,15 +24,25 @@ error Voting_CandidatesRequired();
 error Voting__AlreadyVoted();
 
 contract Voting {
-  struct CandidateDescription {
+  struct Candidate {
+    string id;
     string name;
     string image;
     string party;
-    string voteCount;
+    int16 voteCount;
   }
   address private immutable i_owner;
+  string private constant MAYOR = "Mayor";
+  string private constant DEPUTY_MAYOR = "DeputyMayor";
+  string private constant WARD_CHAIRMAN = "WardChairman";
+  string private constant WARD_MEMBER = "WardMember";
 
   mapping(address => bool) s_voters;
+  mapping(string => mapping(string => Candidate)) s_postToIdToCandidate;
+
+  /** Events */
+  event CandidatesAdded();
+  event VotedSuccessfully(Candidate indexed canidate);
 
   modifier onlyOwner(address user) {
     if (user != i_owner) {
@@ -50,4 +60,39 @@ contract Voting {
   constructor() {
     i_owner = msg.sender;
   }
+
+  ////////////////////////
+  ///// MAIN FUNCTIONS////
+  ////////////////////////
+
+  function setMayorCandidates(Candidate[] memory _candidates) public onlyOwner(msg.sender) {
+    if (_candidates.length < 1) {
+      revert Voting_CandidatesRequired();
+    }
+    for (uint i = 0; i < _candidates.length; i++) {
+      s_postToIdToCandidate[MAYOR][_candidates[i].id] = _candidates[i];
+    }
+    emit CandidatesAdded();
+  }
+
+  function voteMayor(string memory _mayorId) public alreadyVoted(msg.sender) {
+    s_postToIdToCandidate[MAYOR][_mayorId].voteCount += 1;
+    s_voters[msg.sender] = true;
+
+    emit VotedSuccessfully(s_postToIdToCandidate[MAYOR][_mayorId]);
+  }
+
+  // function pickMayorWinner(string[] memory _mayorId) public {
+  //   Candidate[] memory candidates;
+  //   for (uint256 i = 0; i < _mayorId.length; i++) {
+  //     candidates.push(s_postToIdToCandidate[MAYOR][_mayorId[i]]);
+  //   }
+  // }
+
+  ///////////////////////
+  //// GET FUNCTIONS/////
+  ///////////////////////
+  // function getMayorCandidates() public view returns (Candidate memory) {
+  //   return s_postToIdToCandidate[MAYOR];
+  // }
 }
